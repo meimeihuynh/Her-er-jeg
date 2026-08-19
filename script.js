a7d319c8fcdf5471fd1b768a24d1f923364931d9
meimeihuynh <ngohuy02@innlandetfylke.no>
2026-08-19 18:31:28 +0200

    cursor stylization

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ const text = document.getElementById('texttrigger');
 trigger.addEventListener('click', () => {
     text.classList.toggle('collapsed');
 });
+
+/* cursor */
+
+const cursor = document.querySelector('.cursor');
+
+let mouseX = 0;
+let mouseY = 0;
+
+let cursorX = 0;
+let cursorY = 0;
+
+document.addEventListener('mousemove', (event) => {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+});
+
+function animateCursor() {
+    cursorX += (mouseX - cursorX) * 0.12;
+    cursorY += (mouseY - cursorY) * 0.12;
+
+    cursor.style.left = `${cursorX}px`;
+    cursor.style.top = `${cursorY}px`;
+
+    requestAnimationFrame(animateCursor);
+}
+
+animateCursor();
+
